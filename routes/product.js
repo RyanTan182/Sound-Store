637b59e39d2494f4ee83e6c89e9fc722d28171fd
Uwais <192466B@mymail.nyp.edu.sg>
2020-05-23 17:39:52 +0800

    product pg

--- routes/product.js
+++ routes/product.js
@@ -8,3 +8,33 @@ router.get('/listProducts', (req, res) => {
 });
 module.exports = router;
 
+
+//route for the addProduct
+router.get('/addProducts', (req, res) => {
+    res.render('product/addProducts')
+})
+
+// Adds new products from /product/addProducts
+router.post('/addProducts', (req, res) => {
+    let title = req.body.title;
+    let story = req.body.story.slice(0, 1999);
+    let dateRelease = moment(req.body.dateRelease, 'DD/MM/YYYY');
+    let language = req.body.language.toString();
+    let subtitles = req.body.subtitles === undefined ? '' :req.body.subtitles.toString();
+    let classification = req.body.classification;
+    let userId = req.user.id;
+
+    // Multi-value components return array of strings or undefined
+    Video.create({
+        title,
+        story,
+        classification,
+        language,
+        subtitles,
+        dateRelease,
+        userId
+    }) .then(video => {
+        res.redirect('/product/listProducts');
+    })
+    .catch(err => console.log(err))
+});

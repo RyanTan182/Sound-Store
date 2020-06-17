6a05d18aa4d47d0ca5f3e7721598228c4d756a87
Q-Mengxi <191234@mymail.nyp.edu.sg>
2020-06-17 18:23:41 +0800

    hi

--- routes/Delivery.js
+++ routes/Delivery.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get
+router.get('/listDelivery', (req, res) => {
+    res.render('Delivery/listDelivery')
+})
+
+module.exports = router;

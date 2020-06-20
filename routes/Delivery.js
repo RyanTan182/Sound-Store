14f4aa85d94af68a4d070d75a2d0af6642c33d1d
Q-Mengxi <191234@mymail.nyp.edu.sg>
2020-06-20 10:56:30 +0800

    hi

--- routes/Delivery.js
+++ routes/Delivery.js
@@ -5,4 +5,27 @@ router.get('/listDelivery', (req, res) => {
     res.render('Delivery/listDelivery')
 })
 
+router.get('/editDelivery', (req, res) => {
+	res.render('Delivery/editDelivery')
+});
+
+
+router.get('/listdeliveryforuser', (req, res) => {
+	res.render('Delivery/listdeliveryforuse')
+});
+
+router.get('/makedelivery', (req, res) => {
+	res.render('Delivery/makedelivery')
+});
+
+
+router.get('/OrderCheckStaff', (req, res) => {
+	res.render('Delivery/OrderCheckStaff')
+});
+
+router.get('/OrderCheckUser', (req, res) => {
+	res.render('Delivery/OrderCheckUse')
+});
+
+
 module.exports = router;

300387a878e5b7ec5f9bb59cf238b8b4fa09d93c
Uwais <192466B@mymail.nyp.edu.sg>
2020-06-29 15:51:50 +0800

    ll

--- routes/product.js
+++ routes/product.js
@@ -74,20 +74,73 @@ router.get('/edit/:id', (req, res) => {
         }
     }).then((product) => {
         if(!product){
-            alertMessage(res, 'info', 'No such videos', 'fas fa-exclamation-circle', true);
+            alertMessage(res, 'info', 'No such products', 'fas fa-exclamation-circle', true);
         }
         checkOptions(product);
-            // call views/video/editVideo.handlebar to render the edit video page
-        res.render('product/editProduct', {
-            product // passes video object to handlebar
+
+        res.render('product/editProducts', {
+            product
         });
-    }).catch(err => console.log(err)); // To catch no video ID
+    }).catch(err => console.log(err)); 
 });
+
+function checkOptions(product){
+    product.headphones = (product.type.search('Headphones') >= 0) ? 'checked' : '';
+    product.headset = (product.type.search('Headset') >= 0) ? 'checked' : '';
+    product.earbuds = (product.type.search('Earbuds') >= 0) ? 'checked' : '';
+    product.earpiece = (product.type.search('Earpiece') >= 0) ? 'checked' : '';
+}
 
 //route for the optionPage
 router.get('/optionPage', (req, res) => {
     res.render('product/optionPage')
 })
 
+router.get('/delete/:id', (req, res) => {
+    let productId = req.params.id;
+    let userId = req.user.id;
+
+    Product.findOne({
+        where:{
+            id: productId,
+            userId: userId
+        },
+        attributes:['id', 'userId']
+    }).then((product) => {
+        if(product != null){
+            Product.destroy({
+                where:{
+                    id: productId
+                }
+            }).then(() => {
+                alertMessage(res, 'info', 'Product Listing deleted', 'far fa-trash-alt', true);
+                res.redirect('/product/listProducts');
+            }).catch(err => console.log(err));
+        }
+    })
+})
+
+router.get('/browseProducts', (req, res) => {
+    res.render('product/browseProducts')
+})
+
+router.get('/browseProducts', (req, res) => {
+    Product.findAll({
+        where: {
+            userId: req.user.id
+        },
+        order: [
+            ['productTitle', 'ASC']
+        ],
+        raw: true
+    })
+    .then((products) => {
+        res.render('product/browseProducts', {
+            products: products,
+            productImage: products.productImage
+        });
+    })  
+    .catch(err => console.log(err));
+});
 
 module.exports = router;

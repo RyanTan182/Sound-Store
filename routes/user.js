e05c742c337e0e2743e3ca1e0c86a271e81228a2
Ryan Tan <58964130+RyanTan182@users.noreply.github.com>
2020-07-25 19:26:59 +0800

    les

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Staff = require('../models/Staff');
 const alertMessage = require('../helpers/messenger');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const sgMail=require('@sendgrid/mail');
 const jwt=require('jsonwebtoken');
-/* const request=require('request'); */
+const request=require('request');
 const bodyParser=require('body-parser');
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // User register URL using HTTP post => /user/register
 router.post('/registerUser', (req, res) => {
     let errors = [];
 
     // Retrieves fields from register page from request body
-    let {name, email, password, password2} = req.body;
+    let {name, email, password, password2,UserType} = req.body;
 
     // Checks if both passwords entered are the same
     if(password !== password2) {
@@ -33,8 +36,32 @@ router.post('/registerUser', (req, res) => {
             email,
             password,
             password2,
-        });
-    } else {
+            UserType,
+        });}
+    /* if(req.body.captcha===undefined ||
+            req.body.captcha===''||
+            req.body.captcha===null
+        ){
+            return res.json({"success":false,"msg":"Please select captcha"})
+        }
+
+        else {
+        //Secret Key
+        const secretKey='6LeKxbAZAAAAAKLdlRMAGymVM29KdzSHgtbLSAG0';
+        const verifyUrl='https://google.com/recaptcha/api/siteverify?secret=${secretkey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}'
+
+        request(verifyUrl,(err,response,body)=>{
+            body=JSON.parse(body);
+            console.log(body);
+
+            //If not successful
+            if(body.success !== undefined && !body.success){
+                return res.json({"success":false,"msg":"Failed captcha verification"});
+            }
+            //If success
+            return res.json({"success":true,"msg":"Captcha passed"});
+        }) */
+      
         // If all is well, checks if user is already registered
         User.findOne({ where: {email: req.body.email} })
             .then(user => {
@@ -46,154 +73,30 @@ router.post('/registerUser', (req, res) => {
                         name,
                         email,
                         password,
-                        password2
+                        password2,
+                        UserType,
                         });
                 } else {
                     // Create new user record
-                    let token;
-                        jwt.sign(email, 's3cr3Tk3y', (err, jwtoken) => {
-                        if (err) console.log('Error generating Token: ' + err);
-                        token = jwtoken;
-                        });
                     bcrypt.genSalt(10,(err,salt) =>{
                         bcrypt.hash(password,salt,(err,hash) =>{
                             if(err) throw err;
                             password = hash;
-                            User.create({ name, email, password,verified:0, })
+                            User.create({ name, email, password,UserType, })
                             .then(user => {
-                                sendEmail(user.id, user.email, token) // Add this to call sendEmail function
-                                .then(msg => { // Send email success
-                                alertMessage(res, 'success', user.name + ' added. Please logon to ' +
-                                user.email + ' to verify account.',
-                                'fas fa-sign-in-alt', true);
-                                res.redirect('/showLogin');
-                                }).catch(err => { // Send email fail
-                                alertMessage(res, 'warning', 'Error sending to ' + user.email,
-                                'fas fa-sign-in-alt', true);
-                                res.redirect('/');
-                                });
                                 }).catch(err => console.log(err));
                         })
                     });
                 }
             });
         }
-    });
+      
+    );
 
-function sendEmail(userId, email, token){
-    sgMail.setApiKey(yeetus);
-
-    const message = {
-        to: email,
-        from: 'Do Not Reply <admin@video-jotter.sg>',
-        subject: 'Verify Video Jotter Account',
-        text: 'Video Jotter Email Verification',
-        html: `Thank you registering with Video Jotter.<br><br>
-            Please <a href="http://localhost:5000/user/verify/${userId}/${token}">
-                    <strong>verify</strong></a>your account.`
-    };
-    // Returns the promise from SendGrid to the calling function
-    return new Promise((resolve, reject) => {
-        sgMail.send(message)
-            .then(msg => resolve(msg))
-    .catch(err => reject(err));
-    });
-    }
-
-router.get('/verify/:userId/:token', (req, res, next) => {
-    // retrieve from user using id
-    User.findOne({
-    where: {
-    id: req.params.userId
-    }
-    }).then(user => {
-    if (user) { // If user is found
-    let userEmail = user.email; // Store email in temporary variable
-    if (user.verified === true) { // Checks if user has been verified
-    alertMessage(res, 'info', 'User already verified', 'fas faexclamation-circle', true);
-    res.redirect('/showLogin');
-    } else {
-    // Verify JWT token sent via URL
-    jwt.verify(req.params.token, 's3cr3Tk3y', (err, authData) => {
-    if (err) {
-    alertMessage(res, 'danger', 'Unauthorised Access', 'fas faexclamation-circle', true);
-    res.redirect('/');
-    } else {
-    User.update({verified: 1}, {
-    where: {id: user.id}
-    }).then(user => {
-    alertMessage(res, 'success', userEmail + ' verified.Please login', 'fas fa-sign-in-alt', true);
-    res.redirect('/showLogin');
-    });
-    }
-    });
-    }
-} else {
-    alertMessage(res, 'danger', 'Unauthorised Access', 'fas fa-exclamationcircle', true);
-    res.redirect('/');
-    }
-    });
-    });
-
-router.post('/registerStaff', (req, res) => {
-    let errors = [];
-
-    // Retrieves fields from register page from request body
-    let {name, email, password, password2} = req.body;
-
-    // Checks if both passwords entered are the same
-    if(password !== password2) {
-        errors.push({text: 'Passwords do not match'});
-    }
-
-    // Checks that password length is more than 4
-    if(password.length < 4) {
-        errors.push({text: 'Password must be at least 4 characters'});
-    }
-    if (errors.length > 0) {
-        res.render('user/registerStaff', {
-            errors,
-            name,
-            email,
-            password,
-            password2,
-        });
-    } else {
-        // If all is well, checks if user is already registered
-        Staff.findOne({ where: {email: req.body.email} })
-            .then(staff => {
-                if (staff) {
-                // If user is found, that means email has already been
-                    // registered
-                    res.render('user/registerStaff', {
-                        error: staff.email + ' already registered',
-                        name,
-                        email,
-                        password,
-                        password2
-                        });
-                } else {
-                    // Create new user record
-                    bcrypt.genSalt(10,(err,salt) =>{
-                        bcrypt.hash(password,salt,(err,hash) =>{
-                            if(err) throw err;
-                            password = hash;
-                            Staff.create({ name, email, password })
-                            .then(staff => {
-                                alertMessage(res, 'success', staff.name + ' added.Please login', 'fas fa-sign-in-alt', true);
-                                res.redirect('/showLoginStaff');
-                            })
-                            .catch(err => console.log(err));
-                        })
-                    });
-                }
-            });
-        }
-    });
     
 // Login Form POST => /user/login
 router.post('/loginUser', (req, res, next) => {
-    passport.authenticate('local', {
+    passport.authenticate('User', {
         successRedirect: '/user/displayUsers', // Route to /video/listVideos URL
         failureRedirect: '/showLoginUser', // Route to /login URL
         failureFlash: true
@@ -203,30 +106,104 @@ router.post('/loginUser', (req, res, next) => {
     })(req, res, next);
 });
 
-router.post('/loginStaff', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/user/ListUsers', // Route to /video/listVideos URL
-        failureRedirect: '/showLoginStaff', // Route to /login URL
-        failureFlash: true
-        /* Setting the failureFlash option to true instructs Passport to flash an error message using the
-        message given by the strategy's verify callback, if any. When a failure occur passport passes the message
-        object as error */
-    })(req, res, next);
-});
-module.exports = router;
 
 router.post('/forgotPassword', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/user/loginUser', // Route to /video/listVideos URL // Route to /login URL
-        failureRedirect: '/showLoginUser',
-        failureFlash: true
-        /* Setting the failureFlash option to true instructs Passport to flash an error message using the
-        message given by the strategy's verify callback, if any. When a failure occur passport passes the message
-        object as error */
-    })(req, res, next);
-});
+    async.waterfall([
+        function(done) {
+          crypto.randomBytes(20, function(err, buf) {
+            var token = buf.toString('hex');
+            done(err, token);
+          });
+        },
+        function(token, done) {
+          User.findOne({ email: req.body.email }, function(err, user) {
+            if (!user) {
+              req.flash('error', 'No account with that email address exists.');
+              return res.redirect('/forgot');
+            }
+    
+            user.resetPasswordToken = token;
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    
+            user.save(function(err) {
+              done(err, token, user);
+            });
+          });
+        },
+        function(token, user, done) {
+          var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'SendGrid',
+            auth: {
+              user: '!!! YOUR SENDGRID USERNAME !!!',
+              pass: '!!! YOUR SENDGRID PASSWORD !!!'
+            }
+          });
+          var mailOptions = {
+            to: user.email,
+            from: 'passwordreset@demo.com',
+            subject: 'Node.js Password Reset',
+            text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+              'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+              'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+              'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+          };
+          smtpTransport.sendMail(mailOptions, function(err) {
+            req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+            done(err, 'done');
+          });
+        }
+      ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+      });
+    });
 
+    app.post('/reset/:token', function(req, res) {
+        async.waterfall([
+          function(done) {
+            User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+              if (!user) {
+                req.flash('error', 'Password reset token is invalid or has expired.');
+                return res.redirect('back');
+              }
+      
+              user.password = req.body.password;
+              user.resetPasswordToken = undefined;
+              user.resetPasswordExpires = undefined;
+      
+              user.save(function(err) {
+                req.logIn(user, function(err) {
+                  done(err, user);
+                });
+              });
+            });
+          },
+          function(user, done) {
+            var smtpTransport = nodemailer.createTransport('SMTP', {
+              service: 'SendGrid',
+              auth: {
+                user: '!!! YOUR SENDGRID USERNAME !!!',
+                pass: '!!! YOUR SENDGRID PASSWORD !!!'
+              }
+            });
+            var mailOptions = {
+              to: user.email,
+              from: 'passwordreset@demo.com',
+              subject: 'Your password has been changed',
+              text: 'Hello,\n\n' +
+                'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+              req.flash('success', 'Success! Your password has been changed.');
+              done(err);
+            });
+          }
+        ], function(err) {
+          res.redirect('/');
+        });
+      });
 
+      
 router.get('/displayUsers', (req, res, next) => {
     User.findAll({
         where: {
@@ -310,3 +287,5 @@ router.get('/delete/:id', (req, res) => {
         }
     })
 })
+
+module.exports = router ;

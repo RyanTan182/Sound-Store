99a4fe34ca40c7cb87ffcb36b504a9f416d29397
Q-Mengxi <191234@mymail.nyp.edu.sg>
2020-08-16 14:45:32 +0800

    hi

--- routes/Delivery.js
+++ routes/Delivery.js
@@ -4,6 +4,13 @@ const Order = require('../models/order');
 const Addresses = require('../models/address');
 const Deliveryman = require('../models/deliveryman');
 const router = express.Router();
+const Nexmo = require('nexmo');
+const User = require('../models/User');
+
+const nexmo = new Nexmo({
+    apiKey: '599e1242',
+    apiSecret: 'Cx6LjAjj0Kjeh0F4'
+}, {debug: true});
 
 router.get('/listDelivery', (req, res) => {
 	Delivery.findAll({
@@ -31,10 +38,6 @@ router.get('/listdeliveryforuser', (req, res) => {
 	})
 });
 
-router.get('/makedelivery', (req, res) => {
-	res.render('Delivery/makedelivery')
-});
-
 
 router.get('/OrderCheckStaff', (req, res) => {
 	res.render('Delivery/OrderCheckStaff')
@@ -51,22 +54,30 @@ router.post('/OrderCheckStaff/:id',(req, res) =>{
 		}
 	}).then((address) => {
 		//comment : variable that contains what is returned above
-		res.render('Delivery/OrderCheckStaff',{address})
+		console.log(address.userId)
+		Delivery.findOne({
+			where:{
+				userId: address.userId
+			}
+		}).then((delivery)=>{
+			console.log(delivery)
+			console.log(address)
+			res.render('Delivery/OrderCheckStaff',{address,delivery})
+		})
 	})
 });
-
 
 router.post('/createdeliveryman', (req,res) =>{
 	let fname = req.body.fname;
 	let	lname = req.body.lname;
 	let phone = req.body.phone;
-	
 	Deliveryman.create({
 		fname,
 		lname,
 		phone,
-	}).then(deliveries => {
-        res.redirect('/Delivery/makedelivery');
+	}).then(() => {
+		res.redirect('/Delivery/makedelivery');
+		console.log(req.body)
     })
     .catch(err => console.log(err))
 })
@@ -77,7 +88,96 @@ router.get('/createdeliveryman', (req, res) => {
 		raw: true,
 	}).then((deliverymans)=>{
 		res.render('Delivery/createdeliveryman',{deliverymans})
+		console.log(deliverymans)
 	})
 });
 
+router.get('/makedelivery',(req,res) => {
+	console.log('ok')
+	Deliveryman.findAll({
+		order: [['fname', 'ASC']],
+		raw: true, 
+	}).then((deliverymans)=>{
+		res.render('Delivery/makedelivery',{deliverymans})
+		console.log(deliverymans)
+	})
+})
+
+
+router.post('/senddelivery/:id',(req, res) =>{
+	Addresses.findOne({
+		where:{
+			userId: req.params.id
+		},
+		raw: true
+	}).then((address) => {
+		//comment : variable that contains what is returned above
+		Delivery.findOne({
+			where:{
+				userId: address.userId
+			},
+			raw: true
+		}).then(async (delivery) =>{
+			const number = await Deliveryman.findByPk(
+				delivery.DeliverymanID
+			)
+			const phoneNumber = number.phone
+			const text = `the inoformation is:${delivery.productTitle},${address.address}`
+			nexmo.message.sendSms(
+				'6588225004', phoneNumber, text, { type: 'unicode'},
+				(err, responseData) => {
+					if(err){
+						console.log(err);
+					}else{
+						Delivery.update({
+							status: 'Waiting'
+					},{
+							where: {
+								userId : req.params.id
+							}
+							
+						})
+						console.log(delivery.status)
+					}
+				}
+			)
+			res.redirect()
+		})
+	})
+})
+
+router.put('/update/:id',async (req,res)=>{
+	let delivery = await Delivery.findOne({
+		where: {
+			userId: req.params.id
+		}
+	})
+	if (delivery.status =='Waiting' || delivery .status == 'Delivering' || delivery.status == 'Reached'){
+		delivery.status = req.body.status
+		await delivery.save()
+		User.findOne({
+			where:{
+				id : req.params.id
+			},
+			raw: true
+		}).then(async (user) =>{
+			const users = await User.findByPk(
+				user.id
+			)
+			const phoneNumber = users.ContactNo
+			const text = `delivery status changed:${delivery.status}`
+			nexmo.message.sendSms(
+				'6588225004', phoneNumber, text, { type: 'unicode'},
+				(err, responseData) => {
+					if(err){
+						console.log(err);
+					}
+				}
+			)
+		})
+	}
+
+})
+
+//ordered, waiting, delivering, reached
 module.exports = router;
